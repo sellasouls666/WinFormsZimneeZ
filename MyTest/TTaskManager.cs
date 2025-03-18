1a0e583b269e39e374d9abc412f4491c1252dd9d
sellasouls666 <pgbc303@gmail.com>
2025-03-18 12:36:34 +0300

    Сделал тест проверки на то, что выполненная задача не будет возвращаться

--- MyTest/TTaskManager.cs
+++ MyTest/TTaskManager.cs
@@ -11,7 +11,6 @@ namespace MyTest
         [TestMethod]
         public void GetTasksByDate_ReturnsCorrectTasks() // проверка правильного возвращения задач на определённую дату
         {
-            // Arrange
             TaskManager taskManager = new TaskManager();
             DateTime today = DateTime.Today;
             DateTime tomorrow = DateTime.Today.AddDays(1);
@@ -22,10 +21,8 @@ namespace MyTest
             taskManager.AddTask("Сегодняшнее задание 2", today);
             taskManager.AddTask("Вчерашнее задание", yesterday);
 
-            // Act
             List<TaskItem> tasksForToday = taskManager.GetTasksByDate(today);
 
-            // Assert
             Assert.AreEqual(2, tasksForToday.Count);
             Assert.AreEqual("Сегодняшнее задание 1", tasksForToday[0].GetDescription());
             Assert.AreEqual("Сегодняшнее задание 2", tasksForToday[1].GetDescription());
@@ -34,18 +31,31 @@ namespace MyTest
         [TestMethod]
         public void GetTasksByDate_NoTasksForDate_ReturnsEmptyList() //проверка возвращения пустого списка в случае, если нет задач на указанную дату
         {
-            // Arrange
             TaskManager taskManager = new TaskManager();
             DateTime today = DateTime.Today;
             DateTime tomorrow = DateTime.Today.AddDays(1);
 
             taskManager.AddTask("Завтрашнее задание", tomorrow);
 
-            // Act
             List<TaskItem> tasksForToday = taskManager.GetTasksByDate(today);
 
-            // Assert
             Assert.AreEqual(0, tasksForToday.Count);
+        }
+
+        [TestMethod]
+        public void GetTasksByDate_CompletedTasksAreNotReturned() //проверка на то, что выполненная задача не будет возвращаться
+        {
+            TaskManager taskManager = new TaskManager();
+            DateTime today = DateTime.Today;
+
+            taskManager.AddTask("Сегодняшнее задание 1", today);
+            taskManager.AddTask("Сегодняшнее задание 2", today);
+            taskManager.CompleteTask(0);
+
+            List<TaskItem> tasksForToday = taskManager.GetTasksByDate(today);
+
+            Assert.AreEqual(1, tasksForToday.Count);
+            Assert.AreEqual("Сегодняшнее задание 2", tasksForToday[0].GetDescription());
         }
     }
 }
