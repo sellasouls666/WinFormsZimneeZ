b0e4511669416399459a09910a3407cee350e326
sellasouls666 <pgbc303@gmail.com>
2025-03-18 12:18:29 +0300

    Перенёс классы из зимнего задания

---
+++ MyLib/TaskItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class TaskItem
+    {
+        private string description_;
+        private DateTime dueDate_;
+        private bool isCompleted_;
+
+        public TaskItem(string description, DateTime dueDate)
+        {
+            description_ = description;
+            dueDate_ = dueDate;
+            isCompleted_ = false;
+        }
+
+        public DateTime GetDate()
+        {
+            return dueDate_;
+        }
+
+        public bool GetStatus()
+        {
+            return isCompleted_;
+        }
+
+        public string GetDescription()
+        {
+            return description_;
+        }
+
+        public bool SetStatus(bool status)
+        {
+            return isCompleted_ = status;
+        }
+    }
+}
---
+++ MyLib/TaskManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    class TaskManager
+    {
+        private List<TaskItem> tasks = new List<TaskItem>();
+
+        public void AddTask(string description, DateTime dueDate)
+        {
+            tasks.Add(new TaskItem(description, dueDate));
+        }
+
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+            else
+            {
+                throw new Exception("Неверный номер задачи.");
+            }
+        }
+
+        public List<TaskItem> GetTasksByDate(DateTime date)
+        {
+            return tasks.FindAll(task => task.GetDate().Date == date.Date && !task.GetStatus());
+        }
+
+        public void SaveToHtml(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("<html><body><h1>Список задач</h1><ul>");
+                foreach (var task in tasks)
+                {
+                    if (!task.GetStatus())
+                    {
+                        writer.WriteLine($"<li>{task.GetDescription()} - {task.GetDate().ToShortDateString()}</li>");
+                    }
+                }
+                writer.WriteLine("</ul></body></html>");
+            }
+        }
+
+        public void CompleteTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks[index].SetStatus(true);
+            }
+            else
+            {
+                throw new Exception("Неверный номер задачи.");
+            }
+        }
+    }
+}
