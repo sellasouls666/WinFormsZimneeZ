41b4b19dba88b104a05602d0e2b5b13e99ff1dac
sellasouls666 <pgbc303@gmail.com>
2025-03-22 09:35:47 +0300

    Начал менять форму

--- WinFormsZimneeZ/MainForm.Designer.cs
+++ WinFormsZimneeZ/MainForm.Designer.cs
@@ -40,7 +40,9 @@
             this.backButton = new System.Windows.Forms.Button();
             this.completeButton = new System.Windows.Forms.Button();
             this.deleteCompletedButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.tasksTable)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tasksTable
@@ -48,18 +50,18 @@
             this.tasksTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.tasksTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.tasksTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tasksTable.Location = new System.Drawing.Point(12, 12);
+            this.tasksTable.Location = new System.Drawing.Point(12, 191);
             this.tasksTable.MultiSelect = false;
             this.tasksTable.Name = "tasksTable";
             this.tasksTable.ReadOnly = true;
             this.tasksTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.tasksTable.Size = new System.Drawing.Size(776, 210);
+            this.tasksTable.Size = new System.Drawing.Size(675, 210);
             this.tasksTable.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 245);
+            this.label1.Location = new System.Drawing.Point(3, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 1;
@@ -67,7 +69,7 @@
             // 
             // descriptionTextForAdd
             // 
-            this.descriptionTextForAdd.Location = new System.Drawing.Point(125, 242);
+            this.descriptionTextForAdd.Location = new System.Drawing.Point(104, 8);
             this.descriptionTextForAdd.Name = "descriptionTextForAdd";
             this.descriptionTextForAdd.Size = new System.Drawing.Size(210, 20);
             this.descriptionTextForAdd.TabIndex = 2;
@@ -75,7 +77,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(348, 245);
+            this.label2.Location = new System.Drawing.Point(327, 11);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 3;
@@ -84,16 +86,16 @@
             // dateBoxForAdd
             // 
             this.dateBoxForAdd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateBoxForAdd.Location = new System.Drawing.Point(451, 242);
+            this.dateBoxForAdd.Location = new System.Drawing.Point(430, 8);
             this.dateBoxForAdd.Name = "dateBoxForAdd";
             this.dateBoxForAdd.Size = new System.Drawing.Size(83, 20);
             this.dateBoxForAdd.TabIndex = 4;
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(569, 241);
+            this.addButton.Location = new System.Drawing.Point(12, 34);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.Size = new System.Drawing.Size(80, 80);
             this.addButton.TabIndex = 5;
             this.addButton.Text = "Добавить";
             this.addButton.UseVisualStyleBackColor = true;
@@ -101,9 +103,9 @@
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(290, 277);
+            this.deleteButton.Location = new System.Drawing.Point(414, 54);
             this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(198, 23);
+            this.deleteButton.Size = new System.Drawing.Size(59, 40);
             this.deleteButton.TabIndex = 6;
             this.deleteButton.Text = "Удалить задачу";
             this.deleteButton.UseVisualStyleBackColor = true;
@@ -112,16 +114,16 @@
             // dateBoxForFiltr
             // 
             this.dateBoxForFiltr.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateBoxForFiltr.Location = new System.Drawing.Point(27, 317);
+            this.dateBoxForFiltr.Location = new System.Drawing.Point(116, 408);
             this.dateBoxForFiltr.Name = "dateBoxForFiltr";
             this.dateBoxForFiltr.Size = new System.Drawing.Size(92, 20);
             this.dateBoxForFiltr.TabIndex = 7;
             // 
             // filtrButton
             // 
-            this.filtrButton.Location = new System.Drawing.Point(147, 315);
+            this.filtrButton.Location = new System.Drawing.Point(12, 407);
             this.filtrButton.Name = "filtrButton";
-            this.filtrButton.Size = new System.Drawing.Size(98, 23);
+            this.filtrButton.Size = new System.Drawing.Size(95, 21);
             this.filtrButton.TabIndex = 8;
             this.filtrButton.Text = "Отфильтровать";
             this.filtrButton.UseVisualStyleBackColor = true;
@@ -129,9 +131,9 @@
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(266, 317);
+            this.backButton.Location = new System.Drawing.Point(231, 52);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.Size = new System.Drawing.Size(63, 40);
             this.backButton.TabIndex = 9;
             this.backButton.Text = "Сбросить";
             this.backButton.UseVisualStyleBackColor = true;
@@ -139,9 +141,9 @@
             // 
             // completeButton
             // 
-            this.completeButton.Location = new System.Drawing.Point(27, 277);
+            this.completeButton.Location = new System.Drawing.Point(317, 54);
             this.completeButton.Name = "completeButton";
-            this.completeButton.Size = new System.Drawing.Size(218, 23);
+            this.completeButton.Size = new System.Drawing.Size(73, 40);
             this.completeButton.TabIndex = 10;
             this.completeButton.Text = "Выполнить задачу";
             this.completeButton.UseVisualStyleBackColor = true;
@@ -149,36 +151,45 @@
             // 
             // deleteCompletedButton
             // 
-            this.deleteCompletedButton.Location = new System.Drawing.Point(543, 276);
+            this.deleteCompletedButton.Location = new System.Drawing.Point(512, 54);
             this.deleteCompletedButton.Name = "deleteCompletedButton";
-            this.deleteCompletedButton.Size = new System.Drawing.Size(228, 23);
+            this.deleteCompletedButton.Size = new System.Drawing.Size(90, 40);
             this.deleteCompletedButton.TabIndex = 11;
             this.deleteCompletedButton.Text = "Удалить выполненные задачи";
             this.deleteCompletedButton.UseVisualStyleBackColor = true;
             this.deleteCompletedButton.Click += new System.EventHandler(this.deleteCompletedButton_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.deleteButton);
+            this.panel1.Controls.Add(this.deleteCompletedButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.completeButton);
+            this.panel1.Controls.Add(this.descriptionTextForAdd);
+            this.panel1.Controls.Add(this.backButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dateBoxForAdd);
+            this.panel1.Controls.Add(this.addButton);
+            this.panel1.Location = new System.Drawing.Point(12, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(675, 148);
+            this.panel1.TabIndex = 12;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 531);
-            this.Controls.Add(this.deleteCompletedButton);
-            this.Controls.Add(this.completeButton);
-            this.Controls.Add(this.backButton);
+            this.ClientSize = new System.Drawing.Size(710, 440);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tasksTable);
             this.Controls.Add(this.filtrButton);
             this.Controls.Add(this.dateBoxForFiltr);
-            this.Controls.Add(this.deleteButton);
-            this.Controls.Add(this.addButton);
-            this.Controls.Add(this.dateBoxForAdd);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.descriptionTextForAdd);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tasksTable);
             this.Name = "MainForm";
             this.Text = "ToDoList";
             ((System.ComponentModel.ISupportInitialize)(this.tasksTable)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -196,6 +207,7 @@
         private System.Windows.Forms.Button backButton;
         private System.Windows.Forms.Button completeButton;
         private System.Windows.Forms.Button deleteCompletedButton;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
